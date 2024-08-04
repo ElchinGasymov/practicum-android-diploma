84940fbf6adb70168bd30710e960ef60acd65313
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-05 00:56:02 +0300

    Выполнена связка Navigation Graph и Bottom Navigation

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -3,6 +3,8 @@ package ru.practicum.android.diploma.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
@@ -12,6 +14,8 @@ class RootActivity : AppCompatActivity() {
         setContentView(R.layout.activity_root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         val navController = navHostFragment.navController
+        val bottomNavigationMenu =
+            findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
