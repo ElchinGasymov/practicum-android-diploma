2263c68c2ebf7bdfecdca268387b99ec53a3c5ac
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-05 06:58:45 +0300

    Добавлен Binding и скрытие Bottom Navigation

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,21 +1,46 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private val binding: ActivityRootBinding by viewBinding(CreateMethod.INFLATE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavigationMenu =
-            findViewById<BottomNavigationView>(R.id.bottomNavigationView).setupWithNavController(navController)
+            binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            with(binding) {
+                when (destination.id) {
+                    R.id.mainFragment -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+
+                    R.id.favoriteFragment -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+
+                    R.id.teamFragment -> {
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }
+
+                    else -> {
+                        bottomNavigationView.visibility = View.GONE
+                    }
+                }
+            }
+        }
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
