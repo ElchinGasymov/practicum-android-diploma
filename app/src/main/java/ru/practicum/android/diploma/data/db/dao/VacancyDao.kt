ab3d21ed69e6f2b0de2743628e6ec2f270073031
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-06 15:45:09 +0300

    Создан Entity-класс для избранных вакансий.  Созданы DAO-интерфейсы.

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
@@ -1,7 +1,22 @@
 package ru.practicum.android.diploma.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.practicum.android.diploma.data.db.entity.VacancyEntity
 
 @Dao
-interface VacancyDao
+interface VacancyDao {
+    @Query("SELECT * FROM `vacancy_table`")
+    suspend fun getSelectedVacancies(): List<VacancyEntity>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVacancy(vacancy: VacancyEntity)
+
+    @Query("DELETE FROM `vacancy_table` WHERE id=:vacancyId")
+    suspend fun deleteVacancy(vacancyId: Long)
+
+    @Query("SELECT * FROM `vacancy_table` WHERE `vacancy_table`.id = :vacancyId")
+    suspend fun findVacancy(vacancyId: Long): VacancyEntity
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "vacancy_table")
+data class VacancyEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val name: String,
+    val employer: Employer,
+    val salary: Salary?,
+    val area: Area,
+    @SerializedName("alternate_url")
+    val alternateUrl: String,
+    val description: String,
+    val employment: Employment?,
+    val experience: Experience?,
+    val schedule: Schedule?,
+    val contacts: Contacts?,
+    @SerializedName("key_skills")
+    val keySkills: List<KeySkill>,
+    val address: Address?
+)
+
