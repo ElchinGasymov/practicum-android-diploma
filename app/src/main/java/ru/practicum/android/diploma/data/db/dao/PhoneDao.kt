05616ec81fe55724280c54f05aba927404f40e56
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-08 01:41:18 +0300

    Правки

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/PhoneDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/PhoneDao.kt
@@ -12,7 +12,7 @@ interface PhoneDao {
     suspend fun getSelectedPhone(vacancyId: Int): List<PhoneEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPhone(phoneEntity: PhoneEntity)
+    suspend fun insertPhone(phoneEntity: List<PhoneEntity>)
 
     @Query("DELETE FROM `phone_table` WHERE idVacancy=:vacancyId")
     suspend fun deletePhone(vacancyId: Int)
