f85e1213880c607841b83153a9f96cc2d3426a7b
Vitaliy Molodanov <53826004+Logomann@users.noreply.github.com>
2024-08-08 18:49:18 +0300

    Изменен fragment Поиск вакансий, добавлен viewmodel Поиск вакансий, реализована пагинация, добавлена обработка ошибок

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/viewmodels/SearchViewModel.kt
@@ -0,0 +1,107 @@
+package ru.practicum.android.diploma.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.SearchInteractor
+import ru.practicum.android.diploma.ui.state.SearchScreenState
+import ru.practicum.android.diploma.util.Options
+import ru.practicum.android.diploma.util.ResponseData
+
+class SearchViewModel(
+    private val searchInteractor: SearchInteractor
+) : ViewModel() {
+
+    private val screenStateLiveData = MutableLiveData<SearchScreenState>(SearchScreenState.Default)
+    private var currentPage = 0
+    private var maxPages = 1
+    private var isNextPageLoading = false
+    private val itemsPerPage = 20
+    private var mainRequest = ""
+    private var requestNextPage = ""
+
+
+    fun render(): LiveData<SearchScreenState> {
+        return screenStateLiveData
+    }
+
+    private fun setScreenState(state: SearchScreenState) {
+        screenStateLiveData.postValue(state)
+    }
+
+    fun setRequest(request: String) {
+        this.mainRequest = request
+    }
+
+    fun onStart() {
+        setScreenState(SearchScreenState.Default)
+    }
+
+    fun searchVacancies(request: String) {
+        if (request != this.mainRequest) {
+            currentPage = 0
+            setScreenState(SearchScreenState.Loading)
+            this.mainRequest = request
+            requestNextPage = request
+            search(true)
+        }
+
+    }
+
+    private fun search(isNewRequest: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            searchInteractor
+                .search(Options(requestNextPage, itemsPerPage, currentPage))
+                .collect { response ->
+                    when (response) {
+                        is ResponseData.Data -> {
+                            if (response.value.results.isEmpty()) {
+                                setScreenState(SearchScreenState.NothingFound)
+                            } else {
+                                maxPages = response.value.pages
+                                currentPage = response.value.page
+                                if (isNewRequest) {
+                                    setScreenState(
+                                        SearchScreenState.Success(
+                                            response.value.results,
+                                            response.value.foundVacancies
+                                        )
+                                    )
+                                } else {
+                                    setScreenState(
+                                        SearchScreenState.LoadNextPage(
+                                            response.value.results
+                                        )
+                                    )
+                                }
+                            }
+                        }
+
+                        is ResponseData.Error -> {
+                            if (isNewRequest) {
+                                setScreenState(SearchScreenState.Error(response.error))
+                            } else {
+                                setScreenState(SearchScreenState.ErrorNextPage(response.error))
+                            }
+
+                        }
+                    }
+                }
+            isNextPageLoading = false
+        }
+    }
+
+    fun onLastItemReached() {
+        if (!isNextPageLoading) {
+            currentPage++
+            if (currentPage < maxPages) {
+                isNextPageLoading = true
+                setScreenState(SearchScreenState.LoadingNextPage)
+                search(false)
+            }
+        }
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/SearchFragment.kt
@@ -1,19 +1,46 @@
 package ru.practicum.android.diploma.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.presentation.viewmodels.SearchViewModel
+import ru.practicum.android.diploma.ui.state.SearchScreenState
+import ru.practicum.android.diploma.util.DebounceExtension
+import ru.practicum.android.diploma.util.ResponseData
+import ru.practicum.android.diploma.util.adapter.VacancyAdapter
+
+const val TWO_SECONDS = 2000L
+const val VACANCY_KEY = "VACANCY_KEY"
 
 class SearchFragment : Fragment() {
 
     private val binding: FragmentSearchBinding by viewBinding(CreateMethod.INFLATE)
+    private val viewModel by viewModel<SearchViewModel>()
+
+    private val listOfVacancies = ArrayList<Vacancy>()
+    private val adapter = VacancyAdapter({ setOnItemClicked(it) })
+
+    private var amountVacancies = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +53,217 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.onStart()
+        val searchDebounced = DebounceExtension(
+            TWO_SECONDS,
+            { search(binding.searchQuery.text.toString()) },
+            viewLifecycleOwner.lifecycleScope
+        )
+        if (listOfVacancies.isNotEmpty()) {
+            binding.searchDefaultPlaceholder.isVisible = false
+            binding.textUnderSearch.text = getCorrectAmountText(amountVacancies)
+            binding.textUnderSearch.isVisible = true
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null) {
+                    if (s.isNotEmpty()) {
+                        binding.searchIconLoupe.isVisible = false
+                        binding.clearCrossIc.isVisible = true
+                        binding.searchDefaultPlaceholder.isVisible = false
+                        searchDebounced.debounce()
+                    } else {
+                        viewModel.setRequest("")
+                        binding.searchIconLoupe.isVisible = true
+                        binding.clearCrossIc.isVisible = false
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+        binding.searchQuery.addTextChangedListener(textWatcher)
+        binding.clearCrossIc.setOnClickListener {
+            binding.searchQuery.text.clear()
+        }
+
+
+        binding.searchRecycleView.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (dy > 0) {
+                    val pos =
+                        (binding.searchRecycleView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                    val itemsCount = adapter.itemCount
+                    if (pos >= itemsCount - 1) {
+                        viewModel.onLastItemReached()
+                    }
+                }
+            }
+
+        })
+
+        viewModel.render().observe(viewLifecycleOwner) { state ->
+            when (state) {
+                SearchScreenState.Default -> {}
+                is SearchScreenState.Error -> {
+                    when (state.error) {
+                        ResponseData.ResponseError.NO_INTERNET -> {
+                            showNoInternetState()
+                        }
+
+                        ResponseData.ResponseError.CLIENT_ERROR -> {
+                            showServerError()
+                        }
+
+                        ResponseData.ResponseError.SERVER_ERROR -> {
+                            showServerError()
+                        }
+
+                        ResponseData.ResponseError.NOT_FOUND -> {
+                            showNothingFoundState()
+                        }
+                    }
+                }
+
+                is SearchScreenState.LoadNextPage -> {
+                    binding.progressBarNextPage.isVisible = false
+                    listOfVacancies.addAll(state.vacancies)
+                    adapter.setVacancies(listOfVacancies)
+                }
+
+                SearchScreenState.Loading -> {
+                    amountVacancies = 0
+                    listOfVacancies.clear()
+                    adapter.setVacancies(listOfVacancies)
+                    removePlaceholders()
+                    startProgressBar()
+                }
+
+                is SearchScreenState.NothingFound -> {
+                    showNothingFoundState()
+                }
+
+                is SearchScreenState.Success -> {
+                    listOfVacancies.clear()
+                    stopProgressBar()
+                    removePlaceholders()
+                    listOfVacancies.addAll(state.vacancies)
+                    adapter.setVacancies(listOfVacancies)
+                    amountVacancies = state.found
+                    binding.textUnderSearch.text = getCorrectAmountText(amountVacancies)
+                    binding.textUnderSearch.isVisible = true
+                }
+
+                SearchScreenState.LoadingNextPage -> {
+                    binding.progressBarNextPage.isVisible = true
+                }
+
+                is SearchScreenState.ErrorNextPage -> {
+                    binding.progressBarNextPage.isVisible = false
+                    when (state.error) {
+                        ResponseData.ResponseError.NO_INTERNET -> {
+                            showMessage(getString(R.string.next_page_internet_error))
+                        }
+
+                        ResponseData.ResponseError.CLIENT_ERROR -> {
+                            showMessage(getString(R.string.next_page_server_error))
+                        }
+
+                        ResponseData.ResponseError.SERVER_ERROR -> {
+                            showMessage(getString(R.string.next_page_server_error))
+                        }
+
+                        ResponseData.ResponseError.NOT_FOUND -> {
+                            showMessage(getString(R.string.next_page_server_error))
+                        }
+                    }
+                }
+            }
+        }
+        val vacanciesRecyclerView = binding.searchRecycleView
+        vacanciesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        vacanciesRecyclerView.adapter = adapter
 
     }
+
+    private fun search(request: String) {
+        viewModel.searchVacancies(request)
+    }
+
+    private fun removePlaceholders() {
+        binding.textUnderSearch.isVisible = false
+        binding.noVacancyToShow.isVisible = false
+        binding.noVacancyToShowText.isVisible = false
+        binding.noConnectionText.isVisible = false
+        binding.noConnectionPlaceholder.isVisible = false
+        binding.serverError.isVisible = false
+        binding.serverErrorText.isVisible = false
+    }
+
+    private fun startProgressBar() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun stopProgressBar() {
+        binding.progressBar.isVisible = false
+    }
+
+    private fun showNoInternetState() {
+        stopProgressBar()
+        binding.noConnectionPlaceholder.isVisible = true
+        binding.noConnectionText.isVisible = true
+    }
+
+    private fun showNothingFoundState() {
+        stopProgressBar()
+        binding.noVacancyToShow.isVisible = true
+        binding.textUnderSearch.setText(R.string.there_no_such_vacancies)
+        binding.textUnderSearch.isVisible = true
+    }
+
+    private fun showServerError() {
+        stopProgressBar()
+        binding.serverError.isVisible = true
+        binding.serverErrorText.isVisible = true
+    }
+
+    private fun setOnItemClicked(vacancy: Vacancy) {
+        val json = Gson().toJson(vacancy)
+        findNavController().navigate(
+            R.id.action_searchFragment_to_vacancyFragment, bundleOf(
+                VACANCY_KEY to json
+            )
+        )
+    }
+
+    private fun getCorrectAmountText(quantity: Int): String {
+        return requireContext().resources.getQuantityString(
+            R.plurals.vacancy_quantity,
+            quantity,
+            quantity
+        )
+    }
+
+    private fun showMessage(message: String) {
+        val snackbar = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
+            .setTextColor(requireContext().getColor(R.color.white))
+            .setBackgroundTint(requireContext().getColor(R.color.red))
+            .setTextMaxLines(2)
+        snackbar.show()
+        val view = snackbar.view
+        val textView: TextView = view.findViewById(com.google.android.material.R.id.snackbar_text)
+        textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+    }
+
 
 }
