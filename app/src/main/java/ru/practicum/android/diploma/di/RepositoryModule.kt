7205823c5ca977d8616acecc16712b1525d67685
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-09 19:21:15 +0300

    Fix3

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,14 +1,14 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
-import ru.practicum.android.diploma.data.db.SelectedVacanciesRepositoryImpl
+import ru.practicum.android.diploma.data.db.FavouriteVacanciesRepositoryImpl
 import ru.practicum.android.diploma.data.impl.SearchRepositoryImpl
 import ru.practicum.android.diploma.domain.api.SearchRepository
 import ru.practicum.android.diploma.domain.db.FavouriteVacanciesRepository
 
 val repositoryModule = module {
     single<FavouriteVacanciesRepository> {
-        SelectedVacanciesRepositoryImpl(
+        FavouriteVacanciesRepositoryImpl(
             vacanciesDatabase = get(),
             converterIntoModel = get(),
             converterIntoEntity = get()
