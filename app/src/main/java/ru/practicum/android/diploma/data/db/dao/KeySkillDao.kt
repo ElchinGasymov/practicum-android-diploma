955a5e3449ecaf75f1c746d1dd3b39beb1beb3af
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-09 19:11:59 +0300

    Fix2

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/KeySkillDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/KeySkillDao.kt
@@ -9,11 +9,11 @@ import ru.practicum.android.diploma.data.db.entity.KeySkillEntity
 @Dao
 interface KeySkillDao {
     @Query("SELECT * FROM `key_skill_table` WHERE `key_skill_table`.`idVacancy` =:vacancyId")
-    suspend fun getSelectedKeySkill(vacancyId: Int): List<KeySkillEntity>
+    suspend fun getSelectedKeySkill(vacancyId: String): List<KeySkillEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertKeySkill(keySkillEntity: List<KeySkillEntity>)
 
     @Query("DELETE FROM `key_skill_table` WHERE idVacancy=:vacancyId")
-    suspend fun deleteKeySkill(vacancyId: Int)
+    suspend fun deleteKeySkill(vacancyId: String)
 }
--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/PhoneDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/PhoneDao.kt
@@ -9,11 +9,11 @@ import ru.practicum.android.diploma.data.db.entity.PhoneEntity
 @Dao
 interface PhoneDao {
     @Query("SELECT * FROM `phone_table` WHERE `phone_table`.`idVacancy` =:vacancyId")
-    suspend fun getSelectedPhone(vacancyId: Int): List<PhoneEntity>
+    suspend fun getSelectedPhone(vacancyId: String): List<PhoneEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPhone(phoneEntity: List<PhoneEntity>)
 
     @Query("DELETE FROM `phone_table` WHERE idVacancy=:vacancyId")
-    suspend fun deletePhone(vacancyId: Int)
+    suspend fun deletePhone(vacancyId: String)
 }
