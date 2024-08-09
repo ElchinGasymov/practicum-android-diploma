cd88d5f512ddb1edf770224ff0950bb00bfa2d41
Elchin Gasymov <e.gasymov@hh.ru>
2024-08-09 15:06:22 +0300

    Domain слой для деталки по вакансиям

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/DetailsInteractorImpl.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.domain.impl
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.DetailsInteractor
+import ru.practicum.android.diploma.domain.api.VacancyRepository
+import ru.practicum.android.diploma.domain.models.VacancyDetails
+import ru.practicum.android.diploma.util.ResponseData
+
+class DetailsInteractorImpl(private val repository: VacancyRepository) : DetailsInteractor {
+    override fun getVacancy(id: Int): Flow<ResponseData<VacancyDetails>> {
+        return repository.getVacancy(id)
+    }
+}
