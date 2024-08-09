621eaddf9a7fd7c5d0e2d97315b27f154f94bf23
Andrei <116720914+AnShok@users.noreply.github.com>
2024-08-09 15:39:48 +0300

    Fix3...

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancyFragment.kt
@@ -24,7 +24,6 @@ class VacancyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
     }
 
 }
