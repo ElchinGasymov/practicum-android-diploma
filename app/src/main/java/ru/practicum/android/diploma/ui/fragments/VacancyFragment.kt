a8de510e37b5b03171dc38d5eee8db18a0806cc1
Andrei <116720914+AnShok@users.noreply.github.com>
2024-08-09 15:28:54 +0300

    Fix2

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/VacancyFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
