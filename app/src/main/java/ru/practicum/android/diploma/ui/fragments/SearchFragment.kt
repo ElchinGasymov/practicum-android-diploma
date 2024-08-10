18b38744bc6020bfb2f883ef6b6250b4a37341fa
Elchin Gasymov <e.gasymov@hh.ru>
2024-08-10 11:28:15 +0300

    Изменил логику дебаунса, заменил на новый в поиске и добавил для открытия вакансии

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/SearchFragment.kt
@@ -9,7 +9,6 @@ import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,8 +23,6 @@ import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.presentation.viewmodels.SearchViewModel
 import ru.practicum.android.diploma.ui.state.SearchScreenState
-import ru.practicum.android.diploma.util.DebounceExtension
-import ru.practicum.android.diploma.util.DebounceExtension.Companion.TWO_SECONDS
 import ru.practicum.android.diploma.util.ResponseData
 import ru.practicum.android.diploma.util.adapter.VacancyAdapter
 
@@ -54,16 +51,13 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.onStart()
-        val searchDebounced = DebounceExtension(
-            TWO_SECONDS,
-            { search(binding.searchQuery.text.toString()) },
-            viewLifecycleOwner.lifecycleScope
-        )
+
         if (listOfVacancies.isNotEmpty()) {
             binding.searchDefaultPlaceholder.isVisible = false
             binding.textUnderSearch.text = getCorrectAmountText(amountVacancies)
             binding.textUnderSearch.isVisible = true
         }
+
         binding.filterIc.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_filterFragment)
         }
@@ -73,7 +67,7 @@ class SearchFragment : Fragment() {
                 binding.searchIconLoupe.isVisible = false
                 binding.clearCrossIc.isVisible = true
                 binding.searchDefaultPlaceholder.isVisible = false
-                searchDebounced.debounce()
+                viewModel.searchDebounce(binding.searchQuery.text.toString())
             } else {
                 viewModel.setRequest("")
                 binding.searchIconLoupe.isVisible = true
@@ -81,9 +75,11 @@ class SearchFragment : Fragment() {
             }
 
         }
+
         binding.clearCrossIc.setOnClickListener {
             binding.searchQuery.text.clear()
         }
+
         binding.searchRecycleView.addOnScrollListener(object : OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
@@ -99,6 +95,7 @@ class SearchFragment : Fragment() {
             }
 
         })
+
         viewModel.render().observe(viewLifecycleOwner) { state ->
             when (state) {
                 SearchScreenState.Default -> {}
@@ -168,14 +165,11 @@ class SearchFragment : Fragment() {
                 }
             }
         }
+
         val vacanciesRecyclerView = binding.searchRecycleView
         vacanciesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         vacanciesRecyclerView.adapter = adapter
 
-    }
-
-    private fun search(request: String) {
-        viewModel.searchVacancies(request)
     }
 
     private fun removePlaceholders() {
@@ -216,6 +210,9 @@ class SearchFragment : Fragment() {
     }
 
     private fun setOnItemClicked(vacancy: Vacancy) {
+        if (viewModel.vacancyIsClickable.value == false) return
+        viewModel.onVacancyClicked()
+
         val json = Gson().toJson(vacancy)
         findNavController().navigate(
             R.id.action_searchFragment_to_vacancyFragment,
