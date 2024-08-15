00bfd05174ac46c4ce71d6f6b32fe2108011f1c3
Andrei <116720914+AnShok@users.noreply.github.com>
2024-08-15 13:48:29 +0300

    Доработан граф навигации И созданы начальные фрагменты

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/FilterRegionFragment.kt
@@ -0,0 +1,33 @@
+package ru.practicum.android.diploma.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import ru.practicum.android.diploma.databinding.FragmentSelectRegionBinding
+
+class FilterRegionFragment : Fragment() {
+
+    private val binding: FragmentSelectRegionBinding by viewBinding(CreateMethod.INFLATE)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.selectRegionToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+}
