38d3ad8db5a4eed019d4b1a44cd4b8f4d6995c3f
AndreyRyabitckiy <ryabickiyandrey@gmail.com>
2024-08-18 23:30:28 +0300

    Fix5

--- app/src/main/java/ru/practicum/android/diploma/data/dto/SaveFiltersSharedPrefsDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/SaveFiltersSharedPrefsDto.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.data.dto
 import ru.practicum.android.diploma.data.dto.components.CountryDto
 import ru.practicum.android.diploma.data.dto.components.IndustriesDto
 import ru.practicum.android.diploma.data.dto.components.RegionDto
+import java.io.Serializable
 
 data class SaveFiltersSharedPrefsDto(
     val industries: IndustriesDto?,
@@ -10,4 +11,8 @@ data class SaveFiltersSharedPrefsDto(
     val region: RegionDto?,
     val currency: Int?,
     val noCurrency: Boolean?
-)
+) : Serializable {
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
