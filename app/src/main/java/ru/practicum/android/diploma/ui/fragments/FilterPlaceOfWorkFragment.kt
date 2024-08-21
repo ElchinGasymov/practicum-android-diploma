44341346273a66a11e5a80682adc8625d6916f99
Vitaliy Molodanov <53826004+Logomann@users.noreply.github.com>
2024-08-21 14:21:12 +0300

    detekt fix5

--- app/src/main/java/ru/practicum/android/diploma/ui/fragments/FilterPlaceOfWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/fragments/FilterPlaceOfWorkFragment.kt
@@ -31,18 +31,16 @@ import ru.practicum.android.diploma.ui.fragments.FilterFragment.Companion.FILTER
 import ru.practicum.android.diploma.ui.fragments.FilterRegionFragment.Companion.REGION_BUNDLE_KEY
 import ru.practicum.android.diploma.ui.fragments.FilterRegionFragment.Companion.REGION_REQUEST_KEY
 import ru.practicum.android.diploma.ui.state.PlaceOfWorkScreenState
-import ru.practicum.android.diploma.util.App.Companion.PLACE_OF_WORK_COUNTRY_KEY
-import ru.practicum.android.diploma.util.App.Companion.PLACE_OF_WORK_KEY
-import ru.practicum.android.diploma.util.App.Companion.PLACE_OF_WORK_REGION_KEY
-import ru.practicum.android.diploma.util.App.Companion.REGION_ID_KEY
+import ru.practicum.android.diploma.util.PLACE_OF_WORK_COUNTRY_KEY
+import ru.practicum.android.diploma.util.PLACE_OF_WORK_KEY
+import ru.practicum.android.diploma.util.PLACE_OF_WORK_REGION_KEY
+import ru.practicum.android.diploma.util.REGION_ID_KEY
 
 class FilterPlaceOfWorkFragment : Fragment() {
     private val binding: FragmentSelectPlaceOfWorkBinding by viewBinding(CreateMethod.INFLATE)
     private val viewModel by viewModel<FilterPlaceOfWorkViewModel>()
-
     private var country = Country("", "")
     private var region = Region("", "", null)
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -54,70 +52,82 @@ class FilterPlaceOfWorkFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUI()
+        observeViewModel()
+    }
 
+    private fun initUI() {
         initButtonListeners()
         initTextBehaviour()
         initResultListeners()
+    }
 
+    private fun observeViewModel() {
         viewModel.render().observe(viewLifecycleOwner) { state ->
             when (state) {
-                is PlaceOfWorkScreenState.CountryName -> {
-                    country = state.country
-                    binding.countryTextInput.setText(state.country.name)
-                    setCountryEndIcon()
-                    setApplyButtonVisible()
-                }
-
-                PlaceOfWorkScreenState.NoCountryName -> {
-                    binding.countryTextInput.text?.clear()
-                    country = Country("", "")
-                    setNoCountryEndIcon()
-                    checkFields()
-                }
-
-                PlaceOfWorkScreenState.NoRegionName -> {
-                    binding.regionTextInput.text?.clear()
-                    region = Region("", "", null)
-                    setNoRegionEndIcon()
-                    checkFields()
-                }
-
-                is PlaceOfWorkScreenState.RegionName -> {
-                    region = state.region
-                    binding.regionTextInput.setText(state.region.name)
-                    setRegionEndIcon()
-                    setApplyButtonVisible()
-                }
-
-                is PlaceOfWorkScreenState.Saved -> {
-                    val jsonCountry = Gson().toJson(state.country)
-                    val jsonRegion = Gson().toJson(state.region)
-                    setFragmentResult(
-                        PLACE_OF_WORK_KEY,
-                        bundleOf(
-                            PLACE_OF_WORK_COUNTRY_KEY to jsonCountry,
-                            PLACE_OF_WORK_REGION_KEY to jsonRegion,
-                        )
-                    )
-                    findNavController().navigateUp()
-                }
-
-                is PlaceOfWorkScreenState.Loaded -> {
-                    if (state.filters.country != null) {
-                        country = state.filters.country
-                        binding.countryTextInput.setText(state.filters.country.name)
-                        setCountryEndIcon()
-                    }
-                    if (state.filters.region != null) {
-                        region = state.filters.region
-                        binding.regionTextInput.setText(state.filters.region.name)
-                        setRegionEndIcon()
-                    }
-                    setApplyButtonVisible()
-                }
+                is PlaceOfWorkScreenState.CountryName -> handleCountryName(state)
+                PlaceOfWorkScreenState.NoCountryName -> handleNoCountryName()
+                PlaceOfWorkScreenState.NoRegionName -> handleNoRegionName()
+                is PlaceOfWorkScreenState.RegionName -> handleRegionName(state)
+                is PlaceOfWorkScreenState.Saved -> handleSavedState(state)
+                is PlaceOfWorkScreenState.Loaded -> handleLoadedState(state)
             }
-
         }
+    }
+
+    private fun handleCountryName(state: PlaceOfWorkScreenState.CountryName) {
+        country = state.country
+        binding.countryTextInput.setText(state.country.name)
+        setCountryEndIcon()
+        setApplyButtonVisible()
+    }
+
+    private fun handleNoCountryName() {
+        binding.countryTextInput.text?.clear()
+        country = Country("", "")
+        setNoCountryEndIcon()
+        checkFields()
+    }
+
+    private fun handleNoRegionName() {
+        binding.regionTextInput.text?.clear()
+        region = Region("", "", null)
+        setNoRegionEndIcon()
+        checkFields()
+    }
+
+    private fun handleRegionName(state: PlaceOfWorkScreenState.RegionName) {
+        region = state.region
+        binding.regionTextInput.setText(state.region.name)
+        setRegionEndIcon()
+        setApplyButtonVisible()
+    }
+
+    private fun handleSavedState(state: PlaceOfWorkScreenState.Saved) {
+        val jsonCountry = Gson().toJson(state.country)
+        val jsonRegion = Gson().toJson(state.region)
+        setFragmentResult(
+            PLACE_OF_WORK_KEY,
+            bundleOf(
+                PLACE_OF_WORK_COUNTRY_KEY to jsonCountry,
+                PLACE_OF_WORK_REGION_KEY to jsonRegion,
+            )
+        )
+        findNavController().navigateUp()
+    }
+
+    private fun handleLoadedState(state: PlaceOfWorkScreenState.Loaded) {
+        if (state.filters.country != null) {
+            country = state.filters.country
+            binding.countryTextInput.setText(state.filters.country.name)
+            setCountryEndIcon()
+        }
+        if (state.filters.region != null) {
+            region = state.filters.region
+            binding.regionTextInput.setText(state.filters.region.name)
+            setRegionEndIcon()
+        }
+        setApplyButtonVisible()
     }
 
     private fun initResultListeners() {
@@ -137,6 +147,7 @@ class FilterPlaceOfWorkFragment : Fragment() {
             getCountryName(region)
             viewModel.setRegionName(region)
         }
+
         setFragmentResultListener(FILTER_TO_PLACE_OF_WORK_KEY) { _, bundle ->
             val json = bundle.getString(FILTER_TO_PLACE_OF_WORK_COUNTRY_KEY).toString()
             val type = object : TypeToken<Country>() {}.type
@@ -144,14 +155,18 @@ class FilterPlaceOfWorkFragment : Fragment() {
             val jsonRegion = bundle.getString(FILTER_TO_PLACE_OF_WORK_REGION_KEY).toString()
             val typeRegion = object : TypeToken<Region>() {}.type
             region = Gson().fromJson(jsonRegion, typeRegion)
-            if (country.name.isNotEmpty()) {
-                binding.countryTextInput.setText(country.name)
-                setCountryEndIcon()
-                setApplyButtonVisible()
-                if (region.name.isNotEmpty()) {
-                    binding.regionTextInput.setText(region.name)
-                    setRegionEndIcon()
-                }
+            handleLoadedFilters()
+        }
+    }
+
+    private fun handleLoadedFilters() {
+        if (country.name.isNotEmpty()) {
+            binding.countryTextInput.setText(country.name)
+            setCountryEndIcon()
+            setApplyButtonVisible()
+            if (region.name.isNotEmpty()) {
+                binding.regionTextInput.setText(region.name)
+                setRegionEndIcon()
             }
         }
     }
@@ -159,13 +174,8 @@ class FilterPlaceOfWorkFragment : Fragment() {
     private fun initButtonListeners() {
         binding.selectPlaceOfWorkToolbar.setNavigationOnClickListener { findNavController().navigateUp() }
         binding.applyButton.setOnClickListener { saveFilters() }
-
-        binding.countryTextInput.setOnClickListener {
-            navigateToCountrySelection()
-        }
-        binding.regionTextInput.setOnClickListener {
-            navigateToRegionSelection()
-        }
+        binding.countryTextInput.setOnClickListener { navigateToCountrySelection() }
+        binding.regionTextInput.setOnClickListener { navigateToRegionSelection() }
     }
 
     private fun getCountryName(region: Region) {
@@ -174,18 +184,10 @@ class FilterPlaceOfWorkFragment : Fragment() {
 
     private fun initTextBehaviour() {
         binding.countryTextInput.doOnTextChanged { text, _, _, _ ->
-            if (text.isNullOrEmpty()) {
-                updateCountryHintAppearance(false)
-            } else {
-                updateCountryHintAppearance(true)
-            }
+            updateCountryHintAppearance(text?.isNotEmpty() == true)
         }
         binding.regionTextInput.doOnTextChanged { text, _, _, _ ->
-            if (text.isNullOrEmpty()) {
-                updateRegionHintAppearance(false)
-            } else {
-                updateRegionHintAppearance(true)
-            }
+            updateRegionHintAppearance(text?.isNotEmpty() == true)
         }
     }
 
@@ -196,38 +198,28 @@ class FilterPlaceOfWorkFragment : Fragment() {
     private fun setNoCountryEndIcon() {
         binding.countryLayout.apply {
             setEndIconDrawable(R.drawable.ic_arrow_forward_14px)
-            setEndIconOnClickListener {
-                navigateToCountrySelection()
-            }
+            setEndIconOnClickListener { navigateToCountrySelection() }
         }
     }
 
     private fun setCountryEndIcon() {
         binding.countryLayout.apply {
             setEndIconDrawable(R.drawable.ic_close_cross_14px)
-            setEndIconOnClickListener {
-                viewModel.setCountryName(Country("", ""))
-            }
+            setEndIconOnClickListener { viewModel.setCountryName(Country("", "")) }
         }
     }
 
     private fun setNoRegionEndIcon() {
         binding.regionLayout.apply {
             setEndIconDrawable(R.drawable.ic_arrow_forward_14px)
-            setEndIconOnClickListener {
-                navigateToRegionSelection()
-            }
+            setEndIconOnClickListener { navigateToRegionSelection() }
         }
     }
 
     private fun setRegionEndIcon() {
         binding.regionLayout.apply {
             setEndIconDrawable(R.drawable.ic_close_cross_14px)
-            setEndIconOnClickListener {
-                viewModel.setRegionName(
-                    Region("", "", null)
-                )
-            }
+            setEndIconOnClickListener { viewModel.setRegionName(Region("", "", null)) }
         }
     }
 
@@ -243,7 +235,6 @@ class FilterPlaceOfWorkFragment : Fragment() {
     private fun updateCountryHintAppearance(isFilled: Boolean) {
         val isDarkTheme =
             resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
-
         if (isFilled) {
             binding.countryLayout.setHintTextAppearance(R.style.text_12_regular_400)
             binding.countryLayout.defaultHintTextColor = ColorStateList.valueOf(
@@ -282,3 +273,4 @@ class FilterPlaceOfWorkFragment : Fragment() {
         )
     }
 }
+
